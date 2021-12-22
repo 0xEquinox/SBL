0f096484252ee72a44609790e8ebaf08951aada7
0xEquinox <WilliamJMcdonald@protonmail.com>
2021-12-22 01:20:27 -0800

    Re worked the pop code to use pattern matching

--- src/lexer.rs
+++ src/lexer.rs
@@ -61,34 +61,34 @@ impl<'a> Lexer <'a>{
                 //check for pop command
                 '.' => {
 
-                    //Check if the next character is s(stack) or " (for printing)
-
                     self.pos += 1;
 
-                    if  self.current_char() == 's' {
-                        self.pos += 1;
-                        //print the stack
-                        println!("{:?}", self.stack);
-                    //Check if it's a print command
-                    } else if self.current_char() == '"' {
-                        //Take the following string and print it to the console
-                        self.pos += 1;
-
-                        let mut string = String::new();
-
-                        while self.current_char() != '"' {
-                            string.push(self.current_char());
+                    //Check if the next character is s(stack) or " (for printing)
+                    match self.current_char() {
+                        's' => {
                             self.pos += 1;
-                        }
+                            println!("{:?}", self.stack);
+                        },
 
-                        self.pos += 1;
+                        '"' => {
+                            self.pos += 1;
+                            let mut string = String::new();
+                            while self.current_char() != '"' {
+                                string.push(self.current_char());
+                                self.pos += 1;
+                            }
+                            self.stack.push(string.parse::<i64>().unwrap());
+                        },
 
-                        println!("{}", string);
-                    //If there isn't an s just pop the stack
-                    } else if !self.stack.is_empty() {
-                        println!("{}", self.stack.pop().unwrap());
-                    } else{
-                        panic!("Stack is empty");
+                        //The default case is to just pop the stack
+                        _ => {
+                            //If the stack is empty then we can't pop anything
+                            if !self.stack.is_empty() {
+                                self.stack.pop();
+                            } else {
+                                panic!("Stack is empty");
+                            }
+                        },
                     }
                 },
 
